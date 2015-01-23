97b3e504437e18e74b6db9e27eb405be811d729a
Jessica <jessicakoch136@icloud.com>
2015-01-23 14:13:49 -0700

    add faq, social networks, terms and about pages with associated links

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   get 'about' => 'about#index'
   get 'terms' => 'terms#index'
-  get 'faq' => 'faq#index'
+  get 'faq' => 'common_questions#index'
   get 'tasks' => 'tasks#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
