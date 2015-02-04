05d5edc3936039656d03c941e49ba056251a08e9
Jessica <jessicakoch136@icloud.com>
2015-02-04 01:49:46 -0700

    add user page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :users
   root 'welcome#index'
 
   # Example of regular route:
