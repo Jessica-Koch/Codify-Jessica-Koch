044f9f1ea9be1b5b183f6e2445cede8d1bfefc05
Jessica <jessicakoch136@icloud.com>
2015-02-17 16:40:57 -0700

    fix user whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true 
+  validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  
   def full_name
     "#{first_name} #{last_name}"
-end
+  end
+
 end
