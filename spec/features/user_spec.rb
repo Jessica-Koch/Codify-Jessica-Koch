a2c11347c60554f342622dc008d28c9d6feb7c2a
Jessica <jessicakoch136@icloud.com>
2015-03-02 19:45:10 -0700

    user can sign in with valid credentials and cannot signin with invalid credentials

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,30 +1,25 @@
 require 'rails_helper'
 
 
-describe 'User can CRUD users' do
+describe 'User can CRUD users and Sessions work' do
 
   scenario 'User can create a User and see show page' do
-
     visit '/'
-
     click_on "Sign Up"
-
     fill_in 'user[first_name]', with: "Jessica"
     fill_in 'user[last_name]', with: "Koch"
     fill_in 'user[email]', with: "jessicakoch136@icloud.com"
     fill_in 'user[password]', with: "a"
     fill_in 'user[password_confirmation]', with: "a"
     click_on "Register!"
-
     expect(page).to have_content("Jessica")
     click_on  'Jessica'
     expect(page).to have_content("Jessica")
-
     end
 
-  scenario 'User can visit a show page for a user' do
-    User.create(first_name: "Jessica", last_name: "Koch", email: "jessicakoch136@icloud.com", password: "a", password_confirmation: "a")
 
+   scenario 'User can visit a show page for a user' do
+    User.create(first_name: "Jessica", last_name: "Koch", email: "jessicakoch136@icloud.com", password: "a", password_confirmation: "a")
     visit '/'
     click_on 'Sign In'
     fill_in 'email', :with =>"jessicakoch136@icloud.com"
@@ -32,14 +27,12 @@ describe 'User can CRUD users' do
     click_button "Sign In"
     visit '/'
     expect(page).to have_content("Jessica")
-
     click_on "Jessica Koch"
     expect(page).to have_content("Jessica")
-
    end
 
-  scenario 'User cannot leave field blank' do
 
+   scenario 'User cannot leave field blank' do
      visit '/'
      click_on "Sign Up"
      fill_in 'user[first_name]', with: "Jessica"
@@ -48,7 +41,6 @@ describe 'User can CRUD users' do
      fill_in 'user[password]', with: "a"
      fill_in 'user[password_confirmation]', with: "a"
      click_on "Register!"
-
      expect(page).to have_content("error prohibited this object from being saved")
    end
 
@@ -63,22 +55,16 @@ describe 'User can CRUD users' do
      fill_in 'user[password]', with: "a"
      fill_in 'user[password_confirmation]', with: "a"
      click_on "Register!"
-
      expect(page).to have_content("Jessica")
-
      click_on "Jessica"
      click_on "Edit"
-
      fill_in('user[first_name]', :with => 'Jess')
      click_on "Update User"
-
      expect(page).to have_content("Jess")
-
-
    end
 
-   scenario 'User can Delete a User' do
 
+   scenario 'User can Delete a User' do
      visit '/'
      click_on "Sign Up"
      fill_in 'user[first_name]', with: "Jessica"
@@ -90,10 +76,29 @@ describe 'User can CRUD users' do
      expect(page).to have_content("Jessica")
      click_on "Jessica"
      click_on "Edit"
-
      click_on "Delete"
-
      expect(page).to have_no_content("Jessica")
    end
 
+
+   scenario 'User can Signin' do
+    User.create(first_name: "Jessica", last_name: "Koch", email: "jessicakoch136@icloud.com", password: "a", password_confirmation: "a")
+    visit '/'
+    click_on 'Sign In'
+    fill_in 'email', :with =>"jessicakoch136@icloud.com"
+    fill_in 'password', :with => "a"
+    click_button "Sign In"
+    visit '/'
+    expect(page).to have_content("Jessica")
+  end
+
+  scenario 'User cannot signin with invalid credentials' do
+    User.create(first_name: "Jessica", last_name: "Koch", email: "jessicakoch136@icloud.com", password: "a", password_confirmation: "a")
+    visit '/'
+    click_on 'Sign In'
+    fill_in 'email', :with =>"jessicakoch136@icloud.com"
+    fill_in 'password', :with => "b"
+    click_button "Sign In"
+    expect(page).to have_content("Username / password combination is invalid")
+  end
 end
