b67029bc27e9a7179b9f938615d9626dee6143b1
Jessica <jessicakoch136@icloud.com>
2015-03-11 15:51:45 -0600

    user cannot be added to a project twice

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,8 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
+  validates :user_id, presence: true
+  validates_uniqueness_of :user_id, :scope => :project_id
+
   enum role: [:member, :owner]
 end
