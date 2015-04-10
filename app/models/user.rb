18ab41da9b71cf01e9c2fa12045009ea68a1356d
Jessica <jessicakoch136@icloud.com>
2015-04-10 14:27:13 -0600

    users cannot see email for other users, deleted users comments still visible under name 'deleted user'

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,11 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+
   def full_name
-    "#{first_name} #{last_name}"
+    "#{first_name} #{last_name}"    
   end
-  
+
+
 
 end
