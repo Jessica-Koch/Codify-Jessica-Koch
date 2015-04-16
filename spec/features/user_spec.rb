636f0a5bd417ba7c1bc63659015cf0c8a41f85b5
Jessica <jessicakoch136@icloud.com>
2015-04-15 23:58:29 -0600

    user cannot leave field blank while registering

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -17,21 +17,6 @@ describe 'User can CRUD users and Sessions work' do
     expect(page).to have_content("Jessica")
     end
 
-
-   scenario 'User can visit a show page for a user' do
-    User.create(first_name: "Jessica", last_name: "Koch", email: "jessicakoch136@icloud.com", password: "a", password_confirmation: "a")
-    visit '/'
-    click_on 'Sign In'
-    fill_in 'email', :with =>"jessicakoch136@icloud.com"
-    fill_in 'password', :with => "a"
-    click_button "Sign In"
-    visit '/'
-    expect(page).to have_content("Jessica")
-    click_on "Jessica Koch"
-    expect(page).to have_content("Jessica")
-   end
-
-
    scenario 'User cannot leave field blank' do
      visit '/'
      click_on "Sign Up"
@@ -40,8 +25,8 @@ describe 'User can CRUD users and Sessions work' do
      fill_in 'user[email]', with: ""
      fill_in 'user[password]', with: "a"
      fill_in 'user[password_confirmation]', with: "a"
-     click_on "Register!"
-     expect(page).to have_content("error prohibited this object from being saved")
+     click_on "Create User"
+     expect(page).to have_content("error prohibited this form from being saved")
    end
 
 
